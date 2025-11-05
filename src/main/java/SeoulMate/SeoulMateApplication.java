4696ee1125706fac058a8891f646fd9e14b05720
kolk94 <kolk2430@gmail.com>
2025-11-05 18:41:26 +0900

    Initial commit

---
+++ src/main/java/SeoulMate/SeoulMateApplication.java
@@ -0,0 +1,13 @@
+package SeoulMate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeoulMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeoulMateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SeoulMate/SeoulMateApplicationTests.java
@@ -0,0 +1,13 @@
+package SeoulMate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeoulMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
